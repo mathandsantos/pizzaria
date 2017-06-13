08e5409bb7825c5a218b6c6a25f57d5dd6324b06
Andre Lopes <andrelopesramos@outlook.com>
2017-06-13 22:55:08 +0000

    Get LoginR

--- src/Usuario.hs
+++ src/Usuario.hs
@@ -176,4 +176,54 @@ senhaField = Field
             <input type="password" .form-control placeholder="" id=#{idAttr}-confirm name=#{nameAttr} *{otherAttrs} type=textField/>
        |]
    , fieldEnctype = UrlEncoded
-   }   
+   }
+
+getLoginR :: Handler Html
+getLoginR = do
+            (widget, enctype) <- generateFormPost formLogin
+            msgComMaybe <- getMessage
+            defaultLayout $ do 
+                addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                addStylesheet (StaticR style_css)
+                addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+                addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                toWidgetHead [hamlet|
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta name="keywords" content="Teste, Haskell">
+                |]
+                [whamlet|
+                   <header>
+                     <nav .navbar .navbar-default>
+                         <div .container-fluid>
+                             <div .navbar-header>
+                                 <a href=@{HomeR} .navbar-brand .navbar-left>
+                                     Pizzaria Haskeller
+                                 <button type="button" .navbar-toggle .collapsed 
+                                   data-toggle="collapse" data-target="#bs-example-navbar-collapsed-1"
+                                   aria-expanded="false">
+                                      <span .sr-only>
+                                         Toggle Navigation
+                                      <span .icon-bar>
+                                      <span .icon-bar>
+                                      <span .icon-bar>
+                             <div .collapse .navbar-collapse #bs-example-navbar-collapsed-1>
+                                 <ul .nav .navbar-nav .navbar-right>
+                                     <li>
+                                        <a href=@{UsuarioR} .text-center .nav-custom .ye>
+                                           CADASTRAR
+                                     <li>
+                                        <a href=@{LoginR} .text-center .nav-custom .ye>
+                                           LOGAR
+                   <main>
+                    <div .container mar-top>
+                       <div .col-md-6 .col-md-offset-4 .mar-top>
+                          <fieldset>
+                             <h1 .text-center>
+                                Login de usuário
+                             <form method="post" action=@{LoginR}>
+                                 ^{widget}
+                                <div .text-center>
+                                   <button type="submit" .btn .btn-default .btn-submit>
+                                      ENTRAR
+                |]   
