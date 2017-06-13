39572b2c180f579aa0fa9cec21a0ece7848bce37
Matheus A <matheus.academic@gmail.com>
2017-06-13 22:30:09 +0000

    GET da rota PizzaR

--- src/Pedido.hs
+++ src/Pedido.hs
@@ -26,6 +26,25 @@ widgetBebForm x enctype widget y = $(whamletFile "templates/bebida.hamlet")
 
 
 
+getPedidoR :: Handler Html
+getPedidoR = do
+               emailS <- lookupSession "_USER"
+               case emailS of
+                  Nothing -> do
+                     defaultLayout [whamlet| <h1> Usuario não logado.|]
+                  Just x -> do
+                      usS <- runDB $ getBy $ UniqueEmail x
+                      case usS of
+                         Nothing -> do
+                           defaultLayout [whamlet| <h1> erro.|]
+                         Just (Entity usId _) -> do
+                            pedId <-runDB $ insert $ Pedido 0.0 False (toSqlKey(fromSqlKey(usId)))
+                            let a = do toSqlKey(fromSqlKey(pedId))
+                            defaultLayout [whamlet|
+                              <h1> #{show a}  -  Inserido com sucesso. 
+                            |]
+                            redirect $ ListarPedidoR a
+
 postPizzaR :: PedidoId -> Handler Html
 postPizzaR pedId = do
             ((result, _), _) <- runFormPost $ formPiz pedId
